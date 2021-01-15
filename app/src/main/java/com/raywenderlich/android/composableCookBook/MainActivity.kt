5ff51d464bf3c1d6760f61057391c0ebb686dad1
Herve Desrosiers <hdesrosiers@stingray.com>
2021-01-15 09:02:15 -0500

    list of recipes and card padding

--- app/src/main/java/com/raywenderlich/android/composableCookBook/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/composableCookBook/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
 //    setContentView(R.layout.activity_main)
     setContent {
-      Greeting()
+      RecipeList(defaultRecipes)
     }
   }
 }
--- app/src/main/java/com/raywenderlich/android/composableCookBook/RecipeList.kt
+++ app/src/main/java/com/raywenderlich/android/composableCookBook/RecipeList.kt
@@ -1,2 +1,14 @@
 package com.raywenderlich.android.composableCookBook
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RecipeList(recipes: List<Recipe>) {
+    LazyColumnFor(items = recipes) { item ->
+        RecipeCard(item, Modifier.padding(16.dp))
+    }
+}
